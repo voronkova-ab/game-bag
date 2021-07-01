d8a62e7ec8ec0a1e8cfa1298941064ec5a3580c5
Анастасия Воронкова <voronkova__ab@mail.ru>
2021-07-01 18:53:31 +0500

    Добавила функцию создания карточного стола, сделала адаптив

--- script.js
+++ script.js
@@ -1,76 +1,93 @@
-const slides = document.querySelectorAll('.slide');
-const slideMenu = slides[0];
-const slideEasy = slides[1];
-const slideMiddle = slides[2];
-const slideHard = slides[3];
+const slideMenu = document.querySelector('.slide_menu');
+const slide = document.querySelectorAll('.slide')[1];
+const container = document.querySelector('.container');
 const btnStart = document.querySelector('.start-btn');
-const radio = document.querySelectorAll('.list-level__radio');
 const rectangleAll = document.querySelectorAll('.rectangle');
-const cards = document.querySelectorAll('.flip-card');
+const radio = document.querySelectorAll('.list-level__radio');
+let cards;
 
-function assignHendler() {
+function assignHendlerRadio() {
   Array.from(radio).forEach(radio => radio.addEventListener('click', showRectangle));
-  Array.from(cards).forEach(card => card.addEventListener('click', flipCard));
 }
 
-assignHendler();
+assignHendlerRadio();
 
 function showRectangle(event) {
   const rectangleActive = event.target.nextElementSibling.firstElementChild;
   rectangleActive.style.opacity = '1';
 
   switch (event.target.value) {
-     case 'easy':
-       rectangleAll[1].style.opacity = '0';
-       rectangleAll[2].style.opacity = '0';
-       break;
-     case 'middle':
-       rectangleAll[0].style.opacity = '0';
-       rectangleAll[2].style.opacity = '0';
-       break;
-     case 'hard':
-       rectangleAll[0].style.opacity = '0';
-       rectangleAll[1].style.opacity = '0';
-       break;
-     default:
-       console.log('Ошибка');
-   }
+    case 'easy':
+      rectangleAll[1].style.opacity = '0';
+      rectangleAll[2].style.opacity = '0';
+      break;
+    case 'middle':
+      rectangleAll[0].style.opacity = '0';
+      rectangleAll[2].style.opacity = '0';
+      break;
+    case 'hard':
+      rectangleAll[0].style.opacity = '0';
+      rectangleAll[1].style.opacity = '0';
+      break;
+    default:
+      console.log('Ошибка');
+  }
 }
 
 btnStart.addEventListener('click', () => {
   let level = Array.from(radio).filter(item => item.checked);
+
+  if (level.length === 0) return;
+
   level[0].nextElementSibling.firstElementChild.style.opacity = '0';
-  slideMenu.classList.remove('active');
+
+  createCardTable(level[0].value);
   insertWinCard(level[0].value);
 
-  switch (level[0].value) {
-    case 'easy':
-      slideEasy.classList.add('active');
-      break;
-    case 'middle':
-      slideMiddle.classList.add('active');
-      break;
-    case 'hard':
-      slideHard.classList.add('active');
-      break;
-    default:
-    console.log('Ошибка в значении');
-  }
+  slideMenu.classList.remove('active');
+  slide.classList.add('active');
 });
 
-function insertWinCard(level) {
+function createCardTable(level) {
+  let numberCard;
   if (level === 'easy') {
-    const easyCard = slideEasy.querySelectorAll('.flip-card__back');
-    easyCard[getNumberCard(3, 0)].style.backgroundImage = 'url(img/card-win.svg)';
+    numberCard = 3;
+    slide.classList.add('slide_easy');
   }
   if (level === 'middle') {
-    const middleCard = slideMiddle.querySelectorAll('.flip-card__back');
-    middleCard[getNumberCard(6, 0)].style.backgroundImage = 'url(img/card-win.svg)';
+    numberCard = 6;
+    slide.classList.add('slide_middle');
   }
   if (level === 'hard') {
-    const hardCard = slideHard.querySelectorAll('.flip-card__back');
-    hardCard[getNumberCard(10, 0)].style.backgroundImage = 'url(img/card-win.svg)';
+    numberCard = 10;
+    slide.classList.add('slide_hard');
   }
+
+  for (let i = 0; i < numberCard; i++) {
+    const flipCard = document.createElement('div');
+    flipCard.className = 'flip-card';
+    slide.append(flipCard);
+
+    const flipCardFace = document.createElement('img');
+    flipCardFace.src = 'img/card-face.png';
+    flipCardFace.className = 'flip-card__face';
+    flipCard.append(flipCardFace);
+
+    const flipCardBack = document.createElement('img');
+    flipCardBack.src = 'img/card-back.png';
+    flipCardBack.className = 'flip-card__back';
+    flipCard.append(flipCardBack);
+  }
+
+  cards = document.querySelectorAll('.flip-card');
+  Array.from(cards).forEach(card => card.addEventListener('click', flipCard));
+}
+
+function insertWinCard(level) {
+  const cards = slide.querySelectorAll('.flip-card__back');
+  if (level === 'easy') cards[getNumberCard(3, 0)].src = 'img/card-win.png';
+  if (level === 'middle') cards[getNumberCard(6, 0)].src = 'img/card-win.png';
+  if (level === 'hard') cards[getNumberCard(10, 0)].src = 'img/card-win.png';
 }
 
 function getNumberCard(min, max) {
@@ -86,14 +103,12 @@ function flipCard() {
 }
 
 function makeRestart() {
-  this.parentElement.classList.remove('active');
   slideMenu.classList.add('active');
+  this.parentElement.className = 'slide';
 
-  assignHendler();
+  cards.forEach(card => card.remove());
+
+  assignHendlerRadio();
+
   Array.from(cards).forEach(card => card.removeEventListener('click', makeRestart));
-  unFlipCard();
-}
-
-function unFlipCard() {
-  document.querySelector('.flip').classList.remove('flip');
 }
